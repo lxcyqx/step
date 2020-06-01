3bbeeb66fdced1b32152e2ad1f6ea0e9101f3919
Lucy Qu <quxiaoting2@gmail.com>
2020-06-01 11:32:49 -0400

    factor out loadArrows method in art-gallery and gallery

--- portfolio/src/main/webapp/gallery.js
+++ portfolio/src/main/webapp/gallery.js
@@ -37,19 +37,7 @@ function init() {
 
             //execute code after image is loaded
             newImage.onload = function() {
-                let newNextBtn = document.createElement("img");
-                container.appendChild(newNextBtn);
-                newNextBtn.setAttribute("src", "images/right-arrow.png");
-                newNextBtn.setAttribute("class", "img-btn-next");
-                newNextBtn.setAttribute("onclick", "changeImg(1)");
-                newNextBtn.style.cssText = "right: 20px";
-
-                let newPrevBtn = document.createElement("img");
-                container.appendChild(newPrevBtn);
-                newPrevBtn.setAttribute("src", "images/left-arrow.png");
-                newPrevBtn.setAttribute("class", "img-btn-prev");
-                newPrevBtn.setAttribute("onclick", "changeImg(0)");
-                newPrevBtn.style.cssText = "left: 20px";
+                loadArrows(document, container);
             };
             };
         });
