a234542232eab46e3d969a1f7d680848b9bd1826
Lucy Qu <quxiaoting2@gmail.com>
2020-06-01 19:20:59 -0400

    get comments from server and add to DOM

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -21,38 +21,63 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   
-    private List<String> quotes;
+    private List<String> comments;
 
     @Override
     public void init() {
-        quotes = new ArrayList<>();
-        quotes.add(
-            "A ship in port is safe, but that is not what ships are for. "
-                + "Sail out to sea and do new things. - Grace Hopper");
-        quotes.add("They told me computers could only do arithmetic. - Grace Hopper");
-        quotes.add("A ship in port is safe, but that's not what ships are built for. - Grace Hopper");
-        quotes.add("It is much easier to apologise than it is to get permission. - Grace Hopper");
-        quotes.add("If you can't give me poetry, can't you give me poetical science? - Ada Lovelace");
-        quotes.add("I am in a charming state of confusion. - Ada Lovelace");
-        quotes.add(
-            "The Analytical Engine weaves algebraic patterns, "
-                + "just as the Jacquard loom weaves flowers and leaves. - Ada Lovelace");
-        quotes.add(
-            "Sometimes it is the people no one can imagine anything of "
-                + "who do the things no one can imagine. - Alan Turing");
-        quotes.add("Those who can imagine anything, can create the impossible. - Alan Turing");
+        comments = new ArrayList<String>();
+        comments.add("Cool videos");
+        comments.add("I miss Cancun!");
+        comments.add("Very nostalgic");
     }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String quote = quotes.get((int) (Math.random() * quotes.size()));
-        response.setContentType("text/html;");
-        response.getWriter().println(quote);
+        String json = convertToJsonUsingGson(this.comments);
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+    }
+
+    // private String convertToJson(List<String> comments){
+    //     String json = "{";
+    //     // for (int i = 0; i < messages.size(); i++){
+    //     //     json += "\"" + messages.get(i) + "\"";
+    //     //     json+=
+    //     // }
+    //     json += "\"comments\": [";
+
+    //     json += "{";
+    //     json += "\"comment\": "
+    //     json += "\"" + comments.get(0) + "\"";
+    //     json += "}";
+    //     json += ", ";
+
+    //     json += "{";
+    //     json += "\"comment\": "
+    //     json += "\"" + comments.get(1) + "\"";
+    //     json += "}";
+    //     json += ", ";
+
+    //     json += "{";
+    //     json += "\"comment\": "
+    //     json += "\"" + comments.get(2) + "\"";
+    //     json += "}";
+
+    //     json += "]";
+    //     json += "}"
+    //     return json;
+    // }
+
+    private String convertToJsonUsingGson(List<String> comments){
+        Gson gson = new Gson();
+        String json = gson.toJson(comments);
+        System.out.println(json);
+        return json;
     }
 }
