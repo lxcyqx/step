72e0350a6d8526b4a5127e77bb23756dc359d1a1
Lucy Qu <quxiaoting2@gmail.com>
2020-06-05 19:52:27 +0000

    commenting

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet responsible for deleting comment. */
+/** Servlet responsible for deleting specified comment. */
 @WebServlet("/delete-comment")
 public class DeleteCommentServlet extends HttpServlet {
 
