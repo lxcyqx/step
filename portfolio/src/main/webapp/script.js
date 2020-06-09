2e4eca76a7a037ef89990a159eca4ad0b4317eea
Lucy Qu <quxiaoting2@gmail.com>
2020-06-09 18:21:03 +0000

    add translation box to test on deploy

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -176,3 +176,23 @@ function prevPage() {
 //           resultContainer.innerText = translatedMessage;
 //         });
 // }
+
+function requestTranslation() {
+        const text = document.getElementById('text').value;
+        const languageCode = document.getElementById('language').value;
+
+        const resultContainer = document.getElementById('result');
+        resultContainer.innerText = 'Loading...';
+
+        const params = new URLSearchParams();
+        params.append('text', text);
+        params.append('languageCode', languageCode);
+
+        fetch('/translate', {
+          method: 'POST',
+          body: params
+        }).then(response => response.text())
+        .then((translatedMessage) => {
+          resultContainer.innerText = translatedMessage;
+        });
+      }
