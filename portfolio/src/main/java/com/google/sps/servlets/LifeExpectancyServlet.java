d2e20c6a8d1c0d293fc2a7a1ad8bbba668ea552f
Lucy Qu <lucyqu@Lucys-MacBook-Pro.local>
2020-06-11 11:38:04 -0400

    make pr changes

--- portfolio/src/main/java/com/google/sps/servlets/LifeExpectancyServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LifeExpectancyServlet.java
@@ -1,31 +1,35 @@
 package com.google.sps.servlets;
 
-import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Returns Japan city population as a JSON object, e.g. {"Tokyo": 1000000, "Kyoto": 200000}] */
+import com.google.gson.Gson;
+
+/**
+ * Returns Japan's life expectancy by year as a JSON object, e.g. {"1970": 67,
+ * "1971": 68}]
+ */
 @WebServlet("/life-expectancy")
 public class LifeExpectancyServlet extends HttpServlet {
 
-  /* LinkedHashMap with predictable interation order so larger cities show up on the map first */
   private LinkedHashMap<Integer, Double> lifeExpectancyMap = new LinkedHashMap<>();
 
   @Override
   public void init() {
-    Scanner scanner = new Scanner(getServletContext().getResourceAsStream(
-        "/WEB-INF/jp-life-expectancy.csv"));
+    Scanner scanner = new Scanner(
+        getServletContext().getResourceAsStream("/WEB-INF/jp-life-expectancy.csv"));
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
       String[] cells = line.split(",");
 
       Integer year = Integer.valueOf(cells[0]);
-      if (cells.length > 1){
+      if (cells.length > 1) {
         Double lifetime = Double.valueOf(cells[1]);
         lifeExpectancyMap.put(year, lifetime);
       }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,11 +1,12 @@
-google.charts.load('current', {
-  'packages': ['geochart'],
-  'mapsApiKey': 'AIzaSyC35R1Q1qkQndHm-Ni6isXLbsSCQif3Umg'
+google.charts.load("current", {
+  packages: ["geochart"],
+  mapsApiKey: "AIzaSyC35R1Q1qkQndHm-Ni6isXLbsSCQif3Umg"
 });
-// google.charts.setOnLoadCallback(drawMarkersMap);
 google.charts.setOnLoadCallback(getChart);
 
-
+/**
+ * Gets the appropriate chart given the user selection from the dropdown.
+ */
 function getChart() {
   const chartType = document.getElementById("chart-type").value;
   if (chartType === "GeoMap") {
@@ -13,50 +14,54 @@ function getChart() {
   } else if (chartType === "LineChart") {
     drawChart();
   }
-
 }
+
 /** Create map that includes Japan's city and population information using GeoMap, which makes use of Google Charts API, Maps API, and Geocoding API */
 function drawMarkersMap() {
-  fetch('/japan-population').then(response => response.json()).then((cityPopulations) => {
-    const data = new google.visualization.DataTable();
-    data.addColumn('string', 'City');
-    data.addColumn('number', 'Population');
-    Object.keys(cityPopulations).forEach((city) => {
-      data.addRow([city, cityPopulations[city]]);
+  fetch("/japan-population")
+    .then(response => response.json())
+    .then(cityPopulations => {
+      const data = new google.visualization.DataTable();
+      data.addColumn("string", "City");
+      data.addColumn("number", "Population");
+      Object.keys(cityPopulations).forEach(city => {
+        data.addRow([city, cityPopulations[city]]);
+      });
+
+      const options = {
+        title: "Population of Japan's Largest Cities",
+        region: "JP",
+        displayMode: "markers",
+        colorAxis: { colors: ["blue", "red"] }
+      };
+
+      const geomap = new google.visualization.GeoChart(
+        document.getElementById("chart")
+      );
+      geomap.draw(data, options);
     });
-
-    const options = {
-      title: 'Population of Japan\'s Largest Cities',
-      region: 'JP',
-      displayMode: 'markers',
-      colorAxis: { colors: ['blue', 'red'] }
-    };
-
-    const geomap = new google.visualization.GeoChart(document.getElementById('chart'));
-    geomap.draw(data, options);
-  });
 }
 
-google.charts.load('current', { 'packages': ['corechart'] });
-// google.charts.setOnLoadCallback(drawChart);
+google.charts.load("current", { packages: ["corechart"] });
 
 function drawChart() {
-  console.log("in draw chart");
-  fetch('/life-expectancy').then(response => response.json())
-    .then((lifeExpectancy) => {
+  fetch("/life-expectancy")
+    .then(response => response.json())
+    .then(lifeExpectancy => {
       const data = new google.visualization.DataTable();
-      data.addColumn('string', 'Year');
-      data.addColumn('number', 'Life Expectancy');
-      Object.keys(lifeExpectancy).forEach((year) => {
-        console.log(year + " " + typeof year);
+      data.addColumn("string", "Year");
+      data.addColumn("number", "Life Expectancy");
+      Object.keys(lifeExpectancy).forEach(year => {
         data.addRow([year, lifeExpectancy[year]]);
       });
       const options = {
-        title: 'Japan Life Expectancy',
-        legend: { position: 'bottom' }
+        title: "Japan Life Expectancy",
+        legend: { position: "bottom" }
       };
 
-      const lineChart = new google.visualization.LineChart(document.getElementById('chart'));
+      const lineChart = new google.visualization.LineChart(
+        document.getElementById("chart")
+      );
 
       lineChart.draw(data, options);
     });
@@ -64,9 +69,11 @@ function drawChart() {
 
 /** Fetch quote from server and add to DOM */
 function getRandomQuote() {
-  fetch('/quotes').then(response => response.text()).then((quote) => {
-    document.getElementById('quote-container').innerText = quote;
-  })
+  fetch("/quotes")
+    .then(response => response.text())
+    .then(quote => {
+      document.getElementById("quote-container").innerText = quote;
+    });
 }
 
 let currPage = 0;
@@ -75,25 +82,29 @@ let numCommentsOnPage;
 /** Fetch comments from server and add to DOM */
 function getComments() {
   maxNumComments = document.getElementById("num-comments").value;
-  if (maxNumComments === 'All') {
+  if (maxNumComments === "All") {
     maxNumComments = Number.MAX_VALUE;
   }
   let prevBtn = document.getElementById("prevBtn");
 
   handleFirstPage();
 
-  fetch('/data?num=' + maxNumComments + "&page=" + currPage).then(response => response.json()).then((comments) => {
-    const commentElement = document.getElementById('video-comments-container');
-    commentElement.innerHTML = '';
-    numCommentsOnPage = comments.length;
+  fetch("/data?num=" + maxNumComments + "&page=" + currPage)
+    .then(response => response.json())
+    .then(comments => {
+      const commentElement = document.getElementById(
+        "video-comments-container"
+      );
+      commentElement.innerHTML = "";
+      numCommentsOnPage = comments.length;
 
-    handleNoComments(maxNumComments);
-    handleLastPage(maxNumComments);
+      handleNoComments(maxNumComments);
+      handleLastPage(maxNumComments);
 
-    for (i = 0; i < numCommentsOnPage; i++) {
-      commentElement.appendChild(createCommentElement(comments[i]));
-    }
-  });
+      for (i = 0; i < numCommentsOnPage; i++) {
+        commentElement.appendChild(createCommentElement(comments[i]));
+      }
+    });
 }
 
 /** If on first page of comments, disable previous button*/
@@ -109,14 +120,16 @@ function handleFirstPage() {
 function handleLastPage(maxNumComments) {
   let nextPage = currPage + 1;
   //check for number of comments on next page
-  fetch('/data?num=' + maxNumComments + "&page=" + nextPage).then(response => response.json().then((comments) => {
-    //if no comments on next page, then current page is last page
-    if (comments.length === 0) {
-      nextBtn.disabled = true;
-    } else {
-      nextBtn.disabled = false;
-    }
-  }))
+  fetch("/data?num=" + maxNumComments + "&page=" + nextPage).then(response =>
+    response.json().then(comments => {
+      //if no comments on next page, then current page is last page
+      if (comments.length === 0) {
+        nextBtn.disabled = true;
+      } else {
+        nextBtn.disabled = false;
+      }
+    })
+  );
 }
 
 /** Handles scenario in which page has no comments, either when there are no comments in general or when there are no comments on current page due to page size change */
@@ -125,52 +138,58 @@ function handleNoComments(maxNumComments) {
   if (numCommentsOnPage === 0) {
     //set current page to 0 and fetch comments that would be displayed on first page
     currPage = 0;
-    fetch('/data?num=' + maxNumComments + "&page=" + currPage).then(response => response.json()).then((comments) => {
-      //if there aren't any comments at all, add message
-      if (comments.length === 0) {
-        const commentsContainer = document.getElementById("video-comments-container");
-        const noComment = document.createElement('div');
-        noComment.setAttribute("class", "no-comment");
-        noComment.innerText = "No comments to display."
-        commentsContainer.appendChild(noComment);
-      } else {
-        //display comments on the first page
-        const commentElement = document.getElementById('video-comments-container');
-        commentElement.innerHTML = '';
-        handleFirstPage();
-        handleLastPage(maxNumComments);
-        for (i = 0; i < comments.length; i++) {
-          commentElement.appendChild(createCommentElement(comments[i]));
+    fetch("/data?num=" + maxNumComments + "&page=" + currPage)
+      .then(response => response.json())
+      .then(comments => {
+        //if there aren't any comments at all, add message
+        if (comments.length === 0) {
+          const commentsContainer = document.getElementById(
+            "video-comments-container"
+          );
+          const noComment = document.createElement("div");
+          noComment.setAttribute("class", "no-comment");
+          noComment.innerText = "No comments to display.";
+          commentsContainer.appendChild(noComment);
+        } else {
+          //display comments on the first page
+          const commentElement = document.getElementById(
+            "video-comments-container"
+          );
+          commentElement.innerHTML = "";
+          handleFirstPage();
+          handleLastPage(maxNumComments);
+          for (i = 0; i < comments.length; i++) {
+            commentElement.appendChild(createCommentElement(comments[i]));
+          }
         }
-      }
-    });
+      });
   }
 }
 
 /** Create formatting for comment to be inserted */
 function createCommentElement(comment) {
-  const commentElement = document.createElement('div');
+  const commentElement = document.createElement("div");
   commentElement.setAttribute("id", "video-comments");
   //add delete button
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("class", "delete");
-  deleteButton.addEventListener('click', () => {
+  deleteButton.addEventListener("click", () => {
     deleteComment(comment);
-  })
+  });
   deleteButton.innerText = "Delete";
   commentElement.appendChild(deleteButton);
   //add comment text
-  const commentText = document.createElement('div');
+  const commentText = document.createElement("div");
   commentText.setAttribute("class", "comment-text");
   commentText.innerText = comment.text;
   commentElement.appendChild(commentText);
   //add comment footer
-  const commentFooter = document.createElement('div');
+  const commentFooter = document.createElement("div");
   commentFooter.setAttribute("class", "comment-footer");
-  const footerInfo = document.createElement('div');
+  const footerInfo = document.createElement("div");
   footerInfo.setAttribute("class", "footer-info");
   //if user did not provide name, set name as anonymous
-  if (comment.name.trim() === '') {
+  if (comment.name.trim() === "") {
     footerInfo.innerText = "Anonymous" + " | " + comment.timestamp;
   } else {
     footerInfo.innerText = comment.name + " | " + comment.timestamp;
@@ -182,25 +201,25 @@ function createCommentElement(comment) {
 
 /** Creates an <li> element containing text */
 function createListElement(text) {
-  const liElement = document.createElement('li');
+  const liElement = document.createElement("li");
   liElement.innerText = text;
   return liElement;
 }
 
 /** Deletes all the comments and calls getComments to get comments from API with all comments now deleted*/
 function deleteAll() {
-  const request = new Request('/delete-data', { method: 'POST' });
+  const request = new Request("/delete-data", { method: "POST" });
   fetch(request).then(() => {
-    getComments()
+    getComments();
   });
 }
 
 /** Delete given comment */
 function deleteComment(comment) {
   const params = new URLSearchParams();
-  params.append('id', comment.id);
-  fetch('/delete-comment', { method: 'POST', body: params }).then(() => {
-    getComments()
+  params.append("id", comment.id);
+  fetch("/delete-comment", { method: "POST", body: params }).then(() => {
+    getComments();
   });
 }
 
@@ -209,9 +228,11 @@ function addComment() {
   let text = document.getElementById("comment-box").value;
   let name = document.getElementById("name-box").value;
   //if comment input is empty, can't submit
-  if (text.trim() === '') return;
-  document.getElementById("comment-box").value = '';
-  fetch('add-comment?comment-text=' + text + '&name=' + name, { method: 'POST' });
+  if (text.trim() === "") return;
+  document.getElementById("comment-box").value = "";
+  fetch("add-comment?comment-text=" + text + "&name=" + name, {
+    method: "POST"
+  });
 
   setTimeout(getComments, 500);
 }
