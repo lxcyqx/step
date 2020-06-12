ffe81d66d6b334d23660a567c478497033c06007
Lucy Qu <quxiaoting2@gmail.com>
2020-06-12 15:51:01 +0000

    error check in delete comment servlet

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Key;
@@ -23,16 +25,20 @@ public class DeleteCommentServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long id = Long.parseLong(request.getParameter("id"));
+    UserService userService = UserServiceFactory.getUserService();
     String commentEmail = request.getParameter("email");
     String userEmail = request.getParameter("user");
-    if (commentEmail.equals(userEmail)){
+    if (!commentEmail.equals(userEmail) || !userService.isUserLoggedIn()){
+      System.err.println("ERROR: Cannot delete comment.");
+      return;
+    }
+    try {
+      long id = Long.parseLong(request.getParameter("id"));
       Key commentEntityKey = KeyFactory.createKey("Comment", id);
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       datastore.delete(commentEntityKey);
-    } else {
-      System.err.println("ERROR: Cannot delete comment.");
+    } catch (NumberFormatException e){
+      System.err.println("ERROR: Failed to parse to long.");
     }
-
   }
 }
