8c79e50b55d1af921b5afd87668a0fa90cde409e
Lucy Qu <quxiaoting2@gmail.com>
2020-06-15 16:04:52 +0000

    findmeetingquery implementation passes all tests but one

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,46 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    //mandatory attendees of meeting request
+    Collection<String> attendees = request.getAttendees();
+    //times when at least one of the meeting request attendees is busy
+    ArrayList<TimeRange> unavailableTimes = new ArrayList<TimeRange>();
+    for (Event event : events){
+      //get required attendees for each event
+      Set<String> eventAttendees = event.getAttendees();
+      //if at least one of the meeting request attendees has to attend event, then that block of time becomes unavailable
+      for (String attendee : eventAttendees){
+        if (attendees.contains(attendee)){
+          unavailableTimes.add(event.getWhen());
+          break;
+        }
+      }
+    }
+
+    Collections.sort(unavailableTimes, TimeRange.ORDER_BY_START);
+    //get duration of meeting request
+    long meetingDuration = request.getDuration();
+    
+    Collection<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    int start = TimeRange.START_OF_DAY;
+    //find available times and add to collection
+    for (TimeRange eventTime : unavailableTimes){
+      if (!eventTime.contains(start) && eventTime.start() - start >= meetingDuration){
+        availableTimes.add(TimeRange.fromStartEnd(start, eventTime.start(), false));
+      }
+
+      start = eventTime.end();
+    }
+
+    if (TimeRange.END_OF_DAY - start >= meetingDuration){
+      availableTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+    }
+    return availableTimes;
   }
 }
